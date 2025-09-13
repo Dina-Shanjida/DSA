b04c687059a89681c38a0a79fa91c7a30e931bb8
Dina-Shanjida <140268815+Dina-Shanjida@users.noreply.github.com>
2025-09-13 06:55:04 +0600

    Time: 0 ms (100.00%) | Memory: 28.2 MB (28.36%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,20 @@
+class Solution {
+//Moore's voting algorithm
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0 ;
+        int majority_element;
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(count == 0){
+                majority_element = nums[i] ;
+            }
+            if(nums[i] == majority_element){
+                count ++ ;
+            }
+            else count-- ;
+        }
+
+        return majority_element ;
+        
+    }
+};
